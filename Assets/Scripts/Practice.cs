c745959baa9ad47e7dbd9bd1b0d05818360b0e70
jeonmouse <hyeongsoo.jeon@gmail.com>
2021-06-22 05:15:25 +0900

    add log message

--- Assets/Scripts/Practice.cs
+++ Assets/Scripts/Practice.cs
@@ -7,7 +7,8 @@ public class Practice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // https://dotnet.microsoft.com/download/dotnet/sdk-for-vs-code?utm_source=vs-code&amp;utm_medium=referral&amp;utm_campaign=sdk-install
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
