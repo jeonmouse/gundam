57c01ae27e81f383d4ad841598fdd2a3f098af5b
jeonmouse <hyeongsoo.jeon@gmail.com>
2021-06-29 05:59:40 +0900

    update title ui

--- Assets/Scripts/UI/UITitle.cs
+++ Assets/Scripts/UI/UITitle.cs
@@ -1,30 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class UITitle : MonoBehaviour
 {
-    private Button quitButton;
-
     // Start is called before the first frame update
     void Start()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         
-
-        quitButton = root.Q<Button>("QuitButton");
-        quitButton.clicked += QuitButtonClicked;
-    }
-
-    void QuitButtonClicked()
-    {
-        Application.Quit();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
