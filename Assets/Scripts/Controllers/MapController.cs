056f2502b5817acd44732be7c95466b881997dba
jeonmouse <hyeongsoo.jeon@gmail.com>
2021-10-08 05:39:18 +0900

    update map controller

--- Assets/Scripts/Controllers/MapController.cs
+++ Assets/Scripts/Controllers/MapController.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitController : MonoBehaviour
+public class MapController : MonoBehaviour
 {
+    [SerializeField] private GameObject cursor;
+
     private void Start()
     {
         GameManager.Input.MouseAction -= OnMouseEvent;
         GameManager.Input.MouseAction += OnMouseEvent;
+
+        cursor.SetActive(true);
     }
 
     private void OnMouseEvent(Define.MouseEvent evt)
     {
         switch (evt)
         {
+            case Define.MouseEvent.Hover:
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                cursor.transform.position = new Vector2(Mathf.FloorToInt(mousePos.x), Mathf.FloorToInt(mousePos.y));
+                break;
             case Define.MouseEvent.Click:
                 break;
             case Define.MouseEvent.Hold:
