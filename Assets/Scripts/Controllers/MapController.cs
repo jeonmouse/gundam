79581a4ccbe5f035aeb3f681396d2155140d55e6
jeonmouse <hyeongsoo.jeon@gmail.com>
2021-10-09 12:05:11 +0900

    update map controller

--- Assets/Scripts/Controllers/MapController.cs
+++ Assets/Scripts/Controllers/MapController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MapController : MonoBehaviour
 {
     [SerializeField] private GameObject cursor;
+    [SerializeField] private GameObject testUnit;  // For TEST
 
     private const float mapLeft = -10.0f;
     private const float mapRight = 2.0f;
@@ -15,18 +16,25 @@ public class MapController : MonoBehaviour
     {
         GameManager.Input.MouseAction -= OnMouseEvent;
         GameManager.Input.MouseAction += OnMouseEvent;
+
+        // TEST
+        testUnit.transform.position = new Vector2(-3.0f, -0.375f);
+
     }
 
     private void OnMouseEvent(Define.MouseEvent evt)
     {
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         switch (evt)
         {
             case Define.MouseEvent.Hover:
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                DisplayCursor(mousePos);
-                //cursor.transform.position = new Vector2(Mathf.FloorToInt(mousePos.x), Mathf.FloorToInt(mousePos.y));
+                DisplayCursor(position);
                 break;
             case Define.MouseEvent.Click:
+                Ray2D ray = new Ray2D(position, Vector2.zero);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+                Debug.Log(hit.collider.name);
                 break;
             case Define.MouseEvent.Hold:
                 break;
